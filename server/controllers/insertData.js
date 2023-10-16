863d1e211d666db842924dbe0e9510edc28df746
quanglinh <linhdqph27370@fpt.edu.vn>
2023-10-16 21:13:44 +0700

    update data

--- server/controllers/insertData.js
+++ server/controllers/insertData.js
@@ -17,7 +17,8 @@ const fn = async (product) =>{
         sold: Math.round(Math.random() * 100),
         images: product?.images,
         color: product?.varriant?.find(el => el.label === 'Color')?.varriant[0],
-        thumb: product?.thumb
+        thumb: product?.thumb,
+        totalRatings: Math.round(Math.random() * 5)
     })
 }
 
